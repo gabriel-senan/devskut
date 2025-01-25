2d0d29011058382a16a4848d2624d7ab718822d2
gabriel-senan <gabrielsnnh50@gmail.com>
2025-01-25 19:23:28 -0300

    bugfix no core/Model

--- src/Config.php
+++ src/Config.php
@@ -6,9 +6,9 @@ class Config {
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
-    CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_DATABASE = 'devskut';
+    CONST DB_USER = 'gabriel';
+    const DB_PASS = '123';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
